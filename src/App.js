3d5b40a3cd8f902968c385bfb3c397c44616f369
Unclephil <philipnssien@gmail.com>
2023-12-20 23:13:42 +0100

    resolved checkbox

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import CustomDropdown from "./components/selector/index";
+import CustomDropdown from "./components/selector/index"; // Adjust the path accordingly
 import DynamicOptions from "./components/dynamicOptions/DynamicOptions";
 import "./App.css";
 
 const App = () => {
   const [formData, setFormData] = useState({
-    id: null,
     name: "",
     sector: "",
     agreeToTerms: false,
   });
 
-  const [selectedOption, setSelectedOption] = useState("");
+  const [selectedOption, setSelectedOption] = useState('');
 
   const handleSelect = (option) => {
     setSelectedOption(option);
-    setFormData((prevData) => ({ ...prevData, sector: option.value }));
+    setFormData((prevData) => ({ ...prevData, sector: option }));
   };
 
   useEffect(() => {
-    fetchUserData();
+    fetchDataAsync();
   }, []);
 
-  const fetchUserData = async () => {
+  const fetchDataAsync = async () => {
     try {
       const response = await axios.get("http://localhost:5000/api/get-user");
-
-      if (response.data) {
-        const { _id, name, sector, agreeToTerms } = response.data;
-        setFormData({ id: _id, name, sector, agreeToTerms });
-        setSelectedOption({ value: sector, label: sector });
-      }
+      // Process the response if needed
     } catch (error) {
-      console.error("Error fetching user data:", error);
+      console.error("Error fetching sectors:", error);
     }
   };
 
   const addCustomOptionAsync = async (option) => {
     try {
-      await axios.post("http://localhost:5000/api/add-sector", { name: option.label });
+      await axios.post("http://localhost:5000/api/add-sector", { name: option });
     } catch (error) {
       console.error("Error adding custom option:", error);
     }
@@ -60,20 +54,20 @@ const App = () => {
     }
 
     try {
-      const endpoint = formData.id ? "update-user" : "save-user";
-      const response = await axios.post(`http://localhost:5000/api/${endpoint}`, formData);
+      const response = await axios.post("http://localhost:5000/api/save-user", formData);
       console.log(response.data);
     } catch (error) {
       console.error("Error saving data:", error);
     }
   };
 
+  // Disable the checkbox and button if Name or Sector is not filled
   const isDisabled = !formData.name || !formData.sector;
 
   return (
     <div className="container">
       <div className="form-box">
-        <h1 className="form-header">Let's get you started.</h1>
+      <h1 className="form-header">Lets get you started.</h1>
         <form>
           <div className="name-box">
             <label>Name:</label>
@@ -92,27 +86,26 @@ const App = () => {
             <CustomDropdown
               options={DynamicOptions}
               handleSelect={handleSelect}
-              fetchDataAsync={fetchUserData}
+              fetchDataAsync={fetchDataAsync}
               addCustomOptionAsync={addCustomOptionAsync}
-              selectedOption={selectedOption}
             />
           </div>
           <br />
           <div className="terms-box">
             <label>Agree to Terms:</label>
             <input
-             type="checkbox"
-             name="agreeToTerms"
-             checked={formData.agreeToTerms}
-             onChange={handleInputChange}
-             disabled={!selectedOption} // Set the disabled attribute
+              type="checkbox"
+              name="agreeToTerms"
+              checked={formData.agreeToTerms}
+              onChange={handleInputChange}
+              disabled={!selectedOption} // Set the disabled attribute
             />
           </div>
           <br />
           <button
             onClick={handleSave}
             disabled={isDisabled}
-            className={`save-button ${isDisabled ? "save-button-disabled" : ""}`}
+            className={`save-button ${isDisabled ? 'save-button-disabled' : ''}`}
           >
             Save
           </button>
