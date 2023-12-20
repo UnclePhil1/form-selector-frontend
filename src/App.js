f9d71627405fae8f045c150559d5975ed0bfeb00
Unclephil <philipnssien@gmail.com>
2023-12-20 15:59:03 +0100

    screen readers and keyboard users

--- src/App.js
+++ src/App.js
@@ -1,80 +1,65 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CustomDropdown from "./components/selector/index";
-import dynamicOptions from "./components/dynamicoptions/index"; // Adjust the path accordingly
+import dynamicOptions from "./components/dynamicoptions/index";
 import "./App.css";
 import toast, { Toaster } from "react-hot-toast";
 
 const App = () => {
-  // State to manage form data
   const [formData, setFormData] = useState({
     name: "",
     sector: "",
     agreeToTerms: false,
   });
 
-  // State to manage the selected option in the dropdown
   const [selectedOption, setSelectedOption] = useState("");
-
-  // State to track if the name already exists
   const [isNameExists, setIsNameExists] = useState(false);
 
-  // Fetch user data asynchronously when the component mounts
   useEffect(() => {
     fetchDataAsync();
   }, []);
 
-  // Fetch user data from the server
   const fetchDataAsync = async () => {
     try {
       const response = await axios.get("http://localhost:5000/api/get-user");
       const userData = response.data;
 
       if (userData) {
-        // If user data is available, populate the form
         setFormData(userData);
-        // Set the selected option for the dropdown
         setSelectedOption(userData.sector);
       }
     } catch (error) {
-      // Check for error if User data can't be fetched.
       console.error("Error fetching user data:", error);
       toast.error("Error fetching user data");
     }
   };
 
-  // Add a custom option to the dropdown asynchronously
   const addCustomOptionAsync = async (option) => {
     try {
       await axios.post("http://localhost:5000/api/add-sector", {
         name: option,
       });
-      fetchDataAsync(); // Refetch data to update options
+      fetchDataAsync();
     } catch (error) {
       toast.error("Error adding custom option:", error);
     }
   };
 
-  // Handle input changes in the form
   const handleInputChange = (e) => {
     const { name, value, type, checked } = e.target;
     setFormData((prevData) => ({
       ...prevData,
       [name]: type === "checkbox" ? checked : value,
     }));
-    // Reset the nameExists state when the name is changed
     setIsNameExists(false);
   };
 
-  // Handle selection in the dropdown
   const handleSelect = (option) => {
     setSelectedOption(option);
     setFormData((prevData) => ({ ...prevData, sector: option }));
   };
 
-  // Handle the save button click
   const handleSave = async () => {
-    // Check if required fields are filled
     if (!formData.name || !formData.sector || !formData.agreeToTerms) {
       toast.error("All fields are mandatory");
       return;
@@ -86,7 +71,7 @@ const App = () => {
         formData
       );
 
-      console.log("Server Response:", response.data); // Log the server response
+      console.log("Server Response:", response.data);
 
       if (response.data && response.data.success) {
         toast.success("User data saved successfully!");
@@ -99,52 +84,46 @@ const App = () => {
     }
   };
 
-  // Disable the save button if required fields are not filled
   const isDisabled = !formData.name || !formData.sector;
 
   return (
-    // Main container for the whole Page
     <div className="main-container">
       <div className="form-container">
-        {/* Header for the Form */}
         <h1 className="form-heading">Let's get you started.</h1>
 
-        {/* Form Container itself */}
         <form>
-          {/* Input for the user's name */}
           <div className="name-input-container">
-            <label>Name:</label>
+            <label htmlFor="name">Name:</label>
             <input
               type="text"
+              id="name"
               name="name"
               value={formData.name}
               onChange={handleInputChange}
               className={`custom-input ${isNameExists ? "input-error" : ""}`}
               placeholder="Your Name"
             />
-            {/* Display an error message if the name already exists */}
             {isNameExists && (
               <span className="error-message">Name already exists</span>
             )}
           </div>
 
-          {/* Dropdown for selecting sectors */}
           <div className="sector-selector-container">
-            <label>Sectors:</label>
+            <label htmlFor="sector">Sectors:</label>
             <CustomDropdown
               options={dynamicOptions}
               handleSelect={handleSelect}
               fetchDataAsync={fetchDataAsync}
               addCustomOptionAsync={addCustomOptionAsync}
-              selectedOption={selectedOption} // Pass the selected option to the dropdown
+              selectedOption={selectedOption}
             />
           </div>
 
-          {/* Checkbox for agreeing to terms */}
           <div className="terms-checkbox-container">
-            <label>Agree to Terms:</label>
+            <label htmlFor="agreeToTerms">Agree to Terms:</label>
             <input
               type="checkbox"
+              id="agreeToTerms"
               name="agreeToTerms"
               checked={formData.agreeToTerms}
               onChange={handleInputChange}
@@ -152,26 +131,21 @@ const App = () => {
             />
           </div>
 
-          {/* Save button */}
           <button
             onClick={handleSave}
             disabled={isDisabled}
-            className={`save-button ${
-              isDisabled ? "save-button-disabled" : ""
-            }`}
+            className={`save-button ${isDisabled ? "save-button-disabled" : ""}`}
           >
             Save
           </button>
         </form>
       </div>
 
-      {/* Additional text */}
       <div className="side-text-container">
         <p>"Select the Sectors you are currently involved in."</p>
       </div>
-      {/* Toaster */}
+
       <Toaster position="top-center" />
-      {/* Toaster */}
     </div>
   );
 };
--- src/components/selector/index.jsx
+++ src/components/selector/index.jsx
@@ -1,12 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./style.css";
 
-// CustomDropdown component receives props:
-// - options: An array of available options for the dropdown
-// - handleSelect: Function to handle selection of an option
-// - fetchDataAsync: Asynchronous function to fetch data (executed on component mount)
-// - addCustomOptionAsync: Asynchronous function to add a custom option
-// - selectedOption: Currently selected option
 const CustomDropdown = ({
   options,
   handleSelect,
@@ -14,61 +8,68 @@ const CustomDropdown = ({
   addCustomOptionAsync,
   selectedOption,
 }) => {
-  // State to manage the visibility of the dropdown options
   const [isOpen, setIsOpen] = useState(false);
-
-  // Reference to the dropdown container to detect clicks outside the dropdown
   const dropdownRef = useRef(null);
 
-  // useEffect runs when the component mounts
   useEffect(() => {
-    // Fetch initial data asynchronously
     fetchDataAsync();
 
-    // Add an event listener to detect clicks outside the dropdown and close it
     document.addEventListener("click", handleOutsideClick);
 
-    // Clean up the event listener when the component unmounts
     return () => {
       document.removeEventListener("click", handleOutsideClick);
     };
   }, [fetchDataAsync]);
 
-  // Handles a click on an option in the dropdown
   const handleOptionClick = (option) => {
-    // Call the provided handleSelect function to update the selected option
     handleSelect(option);
-    // Close the dropdown
     setIsOpen(false);
   };
 
-  // Handles a click outside the dropdown to close it
   const handleOutsideClick = (event) => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
       setIsOpen(false);
     }
   };
 
-  // Render the component
   return (
-    // Ref attached to the dropdown container to detect clicks outside
-    <div ref={dropdownRef} className="custom-dropdown-container">
-      {/* Header of the dropdown, clicking it toggles the dropdown visibility */}
+    <div
+      ref={dropdownRef}
+      className="custom-dropdown-container"
+      tabIndex={0} // Keyboard accessibility
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.key === " ") {
+          setIsOpen(!isOpen);
+        }
+      }}
+    >
       <div
         className="custom-dropdown-header"
         onClick={() => setIsOpen(!isOpen)}
+        role="button"
+        tabIndex={0} // Keyboard accessibility
+        onKeyUp={(e) => {
+          if (e.key === "Enter" || e.key === " ") {
+            setIsOpen(!isOpen);
+          }
+        }}
       >
         {selectedOption || "Choose your Sector"}
       </div>
-      {/* Dropdown options are rendered if the dropdown is open */}
       {isOpen && (
         <div className="custom-dropdown-options">
-          {/* Map through options and render each as a clickable option */}
           {options.map((option) => (
             <div
               key={option.value}
               className="custom-dropdown-option"
               onClick={() => handleOptionClick(option.value)}
+              tabIndex={0} // Keyboard accessibility
+              role="button"
+              onKeyUp={(e) => {
+                if (e.key === "Enter" || e.key === " ") {
+                  handleOptionClick(option.value);
+                }
+              }}
             >
               {option.label}
             </div>
